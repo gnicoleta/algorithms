e5d2c9cd393e48c85ec9a3a7cbf3e7f3a8dd0394
gnicoleta <nicoleta.gafitanu@gmail.com>
2019-02-21 14:51:28 +0200

    WIP: Kruskal's algo

--- algos/src/main/java/my_kruskal/MyKruskal.java
+++ algos/src/main/java/my_kruskal/MyKruskal.java
@@ -1,8 +1,11 @@
 package my_kruskal;
 
+import javafx.util.Pair;
+import util_structures.MyPair;
 import util_structures.Triplet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MyKruskal {
 
@@ -14,21 +17,38 @@ public class MyKruskal {
 
     public static void kruskal_mst(KGraph graph) {
         ArrayList<Triplet<KNode, KNode, Integer>> all_edges = new ArrayList<Triplet<KNode, KNode, Integer>>();
+        ArrayList<Triplet<KNode, KNode, Integer>> visited_edges = new ArrayList<Triplet<KNode, KNode, Integer>>();
         ArrayList<KNode> visited_nodes = new ArrayList<KNode>();
 
         for (KNode n : graph.getNodes()) {
             MyKruskal.addEdgesFromAllNodes(n.getEdges(), all_edges);
         }
 
+        ArrayList<MyPair<Triplet<KNode, KNode, Integer>, Integer>> parsed_edges = new ArrayList<MyPair<Triplet<KNode, KNode, Integer>, Integer>>();
+
+        Iterator edge = all_edges.iterator();
+        while (edge.hasNext()) {
+            parsed_edges.add(new MyPair(edge, 0));
+        }
+
+        //while()
         KNode current_node_parent = new KNode();
         KNode current_node = new KNode();
+        MyPair<Triplet<KNode, KNode, Integer>, Integer> currrnt = new MyPair<Triplet<KNode, KNode, Integer>, Integer>();
         Integer MIN = Integer.MAX_VALUE;
-        for (Triplet<KNode, KNode, Integer> t : all_edges) {
-            if (t.getThird() < MIN) {
-                MIN = t.getThird();
-                current_node_parent = t.getFirst();
-                current_node = t.getSecond();
+        for (MyPair<Triplet<KNode, KNode, Integer>, Integer> t : parsed_edges) {
+            if (t.getFirst().getThird() < MIN) {
+                MIN = t.getFirst().getThird();
+                currrnt = t;
+                //current_node_parent = t.getFirst();
+                //current_node = t.getSecond();
             }
+        }
+        currrnt.setSecond(1);
+        visited_edges.add(currrnt.getFirst());
+        if(!visited_nodes.contains(currrnt.getFirst().getFirst()) && !visited_nodes.contains(currrnt.getFirst().getSecond())) {
+            visited_nodes.add(currrnt.getFirst().getFirst());
+            visited_nodes.add(currrnt.getFirst().getSecond());
         }
 
     }
